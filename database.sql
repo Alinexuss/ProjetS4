d50e7dff627a0c195b16e377704088c87ce3725b
jessymihaja <119971173+jessymihaja@users.noreply.github.com>
2023-07-11 11:42:28 +0100

    database update

--- database.sql
+++ database.sql
@@ -31,12 +31,14 @@ CREATE TABLE objectifUtilisateur (
     FOREIGN KEY (idObjectif) REFERENCES objectif(idObjectif) ,
     FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(idUtilisateur) 
 );
+create or replace view objectifUtilisateur_detail as select o.idobjectif,o.typeObjectif,u.idUtilisateur,u.kilo from objectifUtilisateur as u join objectif as o on o.idObjectif=u.idObjectif;
+create or replace view objectifUtilisateur_detail_detail as select o.idobjectif,o.typeObjectif,u.nom,o.kilo from objectifUtilisateur_detail as o join utilisateur as u on o.idUtilisateur=u.idUtilisateur;
 
 -- Donnees sur les programmes 
 CREATE TABLE exercice (
     idExercice INT auto_increment PRIMARY KEY ,
     exercice VARCHAR(25) NOT NULL ,
-    durree TIME NOT NULL ,
+    durree INT NOT NULL ,
     rapportKcal FLOAT NOT NULL
 );
 
@@ -46,11 +48,11 @@ CREATE TABLE seance (
     repetition int,
     FOREIGN KEY (idExercice) REFERENCES exercice(idExercice)
 );
-create or replace view seance_detail as select s.idSceance,e.exercice,s.repetition from seance as s join exercice as e on s.idExercice=e.idExercice;
+create or replace view seance_detail as select s.idSceance,e.exercice,s.repetition,e.rapportKcal from seance as s join exercice as e on s.idExercice=e.idExercice;
 
 CREATE TABLE regime (
     idRegime INT auto_increment PRIMARY KEY ,
-    heure TIME NOT NULL ,
+    heure INT NOT NULL ,
     aliment VARCHAR(255) NOT NULL ,
     rapportKcal FLOAT NOT NULL,
     prixUnitaire float not null
@@ -61,9 +63,38 @@ CREATE TABLE session(
     duree INT,
     FOREIGN KEY (idRegime) REFERENCES regime(idRegime)
 );
-create or replace view session_detail as select s.idSession,r.aliment,s.duree from regime as r join session as s on r.idRegime=s.idRegime;
+create or replace view session_detail as select s.idSession,r.aliment,s.duree,r.rapportKcal from regime as r join session as s on r.idRegime=s.idRegime;
 
-insert into objectif(idObjectif,typeObjectif) values (0,"prendre du poinds");
-insert into objectif(idObjectif,typeObjectif) values (1,"perdre du poinds");
+create or replace view calorie_seance as select (repetition*rapportKcal) as calorie,idsceance from seance_detail;
+
+create or replace view calorie_session as select (duree*rapportKcal) as calorie,idSession from session_detail;
 
 
+insert into objectif(idObjectif,typeObjectif) values (0,"prendre du poids");
+insert into objectif(idObjectif,typeObjectif) values (1,"perdre du poids");
+insert into objectif(idObjectif,typeObjectif) values (2,"atteindre son IMC ideal");
+
+insert into regime(heure,aliment,rapportKcal,prixUnitaire) values (6,'poulet soupe',200,15000);
+insert into regime(heure,aliment,rapportKcal,prixUnitaire) values (12,'legumes sautes',50,10000);
+insert into regime(heure,aliment,rapportKcal,prixUnitaire) values (18,'poisson panee',175,20000);
+insert into regime(heure,aliment,rapportKcal,prixUnitaire) values (18,'viande de boeuf cuit',400,40000);
+insert into regime(heure,aliment,rapportKcal,prixUnitaire) values (12,'fruits rouges',75,5000);
+
+
+insert into exercice (exercice,durree,rapportKcal) values ('gennage',10,-200);
+insert into exercice (exercice,durree,rapportKcal) values ('abdo',15,-300);
+insert into exercice (exercice,durree,rapportKcal) values ('dips',30,150);
+insert into exercice (exercice,durree,rapportKcal) values ('course',60,-150);
+insert into exercice (exercice,durree,rapportKcal) values ('traction',45,-90);
+
+insert into seance(idExercice,repetition) values(1,3);
+insert into seance(idExercice,repetition) values(2,3);
+insert into seance(idExercice,repetition) values(3,5);
+insert into seance(idExercice,repetition) values(4,2);
+insert into seance(idExercice,repetition) values(5,3);
+
+insert into session(idRegime,duree) values (1,10);
+insert into session(idRegime,duree) values (2,12);
+insert into session(idRegime,duree) values (3,15);
+insert into session(idRegime,duree) values (4,7);
+insert into session(idRegime,duree) values (5,8);
